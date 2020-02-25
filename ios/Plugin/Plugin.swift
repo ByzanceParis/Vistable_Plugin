da6ff7fa54c32b3422e558468b3007ef066af6c8
Tom <tom@byzance.world>
2020-02-25 17:52:01 +0100

    plugin Lancome

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -16,6 +16,30 @@ public class VistablePlugin: CAPPlugin {
     var unicId:String?
     var callerFn:CAPPluginCall?
 
+    let colors: [String: Any] = [
+            "0":"255|109|021|060",
+            "1":"195|043|033|050",
+            "2":"255|100|086|040",
+            "3":"120|070|150|130",
+            "4":"120|070|150|190",
+            "5":"160|026|059|100",
+            "6":"255|110|060|130",
+            "7":"110|070|160|145",
+            "8":"200|060|022|140",
+            "9":"098|039|132|170",
+            "10":"255|114|060|150",
+            "11":"202|033|020|200",
+            "12":"255|110|060|130",
+            "13":"250|050|050|160",
+            "14":"120|070|150|130",
+            "15":"080|140|035|250",
+            "16":"055|090|010|250",
+            "17":"100|100|050|255",
+            "18":"240|114|090|250",
+            "19":"255|092|084|080",
+            "20":"168|034|059|250",
+            "21":"220|060|025|130"
+    ]
 
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
@@ -46,12 +70,18 @@ public class VistablePlugin: CAPPlugin {
         let win = call.getString("win") ?? "0"
         let second = call.getString("second") ?? "1"
         let third = call.getString("third") ?? "2"
+        print("Win: " + win)
+        print("Second: " + second)
+        print("Third: " + third)
 //        var ids = call.getArray("ids", Array<Int>.self)
         var stringRepresentation:String = self.unicId! + "&"
 
-        stringRepresentation += win + ":FF0000&"
-        stringRepresentation += second + ":FF0000&"
-        stringRepresentation += third + ":FF0000&"
+        let coul:String = colors[win] as! String
+        print("Coul: " + coul)
+        stringRepresentation += win + ":" + coul + "&"
+        stringRepresentation += second + ":100|100|100|220&"
+        stringRepresentation += third + ":100|100|100|220&"
+        print(stringRepresentation)
 
         self.ledString = stringRepresentation
         self.bleManager?.turnOn(id: stringRepresentation)
